c9b81a701fa497b825e2b06b3c1f674bbe374e4b
이현규 <gusrb2776@naver.com>
2024-09-27 09:31:53 +0900

    add

--- src/main/java/study/snowflakestudy/Category.java
+++ src/main/java/study/snowflakestudy/Category.java
@@ -9,6 +9,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.GenericGenerator;
 import study.snowflakestudy.snowflake.BaseEntity;
 
 @Entity
@@ -16,6 +17,14 @@ import study.snowflakestudy.snowflake.BaseEntity;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class Category extends BaseEntity {
+public class Category{
+
+    @Id
+    // name : GeneratedValue 에서 사용할 이름, strategy : 어떻게 생성할껀지. (IdentifierGenerator 구현한 친구)
+    @GenericGenerator(name ="snowflake_id", strategy = "study.snowflakestudy.snowflake.CustomSnowFlakeGenerator")
+    // 바로위에서 이름 정한애로 사용하는거임.
+    @GeneratedValue(generator = "snowflake_id")
+    private Long id;
+
     private Long test;
 }
